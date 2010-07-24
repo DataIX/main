5598eba0cf7834f9676da2e617781f4e2578193b
J. Hellenthal <jhellenthal@DataIX.net>
2010-07-24 05:51:38 +0000

    This is for ipv4 only.

--- base/projects/tcplist/tcplist.sh
+++ base/projects/tcplist/tcplist.sh
@@ -30,7 +30,7 @@
 
 printf " <->\tUID \tLOCAL:PORT \t\tREMOTE:PORT\n"
 printf " ===\t======  ======================  ======================\n"
-lsof -Pnl -iTCP -sTCP:ESTABLISHED 2>/dev/null |\
+lsof -Pnl -i4 -iTCP -sTCP:ESTABLISHED 2>/dev/null |\
 	awk '{print $3,$(NF - 1)}' |\
 	grep "[0-9]" |sed 's/->/ /' |\
 	awk '{print " \t"$1" \t"$2" \t"$3}' |\
