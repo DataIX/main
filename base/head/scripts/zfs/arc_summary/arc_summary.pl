dd2a1e48171d7790e1fd97234211e25ca555cc4a
J. Hellenthal <jhellenthal@DataIX.net>
2010-08-11 18:33:50 +0000

    Be sure to thank Martin Matuska by adding him to the copyright for the contribution of the DMU code.

--- base/head/scripts/zfs/arc_summary/arc_summary.pl
+++ base/head/scripts/zfs/arc_summary/arc_summary.pl
@@ -2,8 +2,9 @@
 #
 # $Id$
 #
-# Copyright (c) 2008, Ben Rockwood (benr@cuddletech.com)
+# Copyright (c) 2008, Ben Rockwood <benr@cuddletech.com>,
 # Copyright (c) 2010 Jason J. Hellenthal  <jhell@dataix.net>,
+# Copyright (c) 2010 Martin Matuska <mm@FreeBSD.org>,
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
