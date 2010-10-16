51cec88fc652f65e4d5f5b504d7df81e1bdb4c9c
J. Hellenthal <jhell@DataIX.net>
2010-10-16 14:56:07 -0400

    Turn these spaces into tabs for the L2 ARC Buffer added section.

--- base/head/scripts/zfs/arc_summary/arc_summary.pl
+++ base/head/scripts/zfs/arc_summary/arc_summary.pl
@@ -408,10 +408,10 @@ if ($l2_size > 0 & $l2_access_total > 0) {
 		$l2_feeds);
 	print "\n";
 
-        print "L2 ARC Buffer:\n";
-        printf("\tBytes Scanned:\t\t\t\t%dK\n",
-                $l2_write_buffer_bytes_scanned/1024 );
-        print "\n";
+	print "L2 ARC Buffer:\n";
+	printf("\tBytes Scanned:\t\t\t\t%dK\n",
+		$l2_write_buffer_bytes_scanned/1024 );
+	print "\n";
 	
 	print "L2 ARC Writes:\n";
 	if ($l2_writes_done != $l2_writes_sent) {
