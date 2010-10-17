82b30a515984c04e6324b7bedd791f040051a59e
J. Hellenthal <jhell@DataIX.net>
2010-10-16 20:54:24 -0400

    Fix Buffer Iterations to use 100k or 100m for its divisions.

--- base/head/scripts/zfs/arc_summary/arc_summary.pl
+++ base/head/scripts/zfs/arc_summary/arc_summary.pl
@@ -422,11 +422,11 @@ if ($l2_size > 0 & $l2_access_total > 0) {
 	}};
 
 	print "\tBuffer Iterations:\t\t\t";
-	if ($l2_write_buffer_iter > 1000000) {
-		printf("%0.2fm\t(x1000000)\n", $l2_write_buffer_iter /1000000);
+	if ($l2_write_buffer_iter > 100000000) {
+		printf("%0.2fm\t*(100m)\n", $l2_write_buffer_iter /100000000);
 	} else { if ($l2_write_buffer_iter > 100000) {
-			printf("%0.2fk\t(x100000)\n", $l2_write_buffer_iter /100000);
-	} else { printf("%d\t(x1)\n", $l2_write_buffer_iter );
+			printf("%0.2fk\t*(100k)\n", $l2_write_buffer_iter /100000);
+	} else { printf("%d\t*(1)\n", $l2_write_buffer_iter );
 	}};
 
 	print "\tList Iterations:\t\t\t";
