f77af271112edb0d644c2cd309eace9f7d47c9f0
J. Hellenthal <jhell@DataIX.net>
2010-10-16 21:02:34 -0400

    Remove some extra spacing thats not needed.

--- base/head/scripts/zfs/arc_summary/arc_summary.pl
+++ base/head/scripts/zfs/arc_summary/arc_summary.pl
@@ -444,8 +444,6 @@ if ($l2_size > 0 & $l2_access_total > 0) {
 			printf("%0.2fk\t*(100k)\n", $l2_write_buffer_list_null_iter/100000);
 	} else { printf("%d\t*(1)\n", $l2_write_buffer_list_null_iter);
 	}};
-
-
 	print "\n";
 	
 	print "L2 ARC Writes:\n";
