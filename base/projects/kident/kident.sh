c8d1613c646d62f2eef466a899ecedff14d9ccbd
J. Hellenthal <jhellenthal@DataIX.net>
2011-01-07 06:57:24 -0500

    Revert arc_summary.pl to use direct path /usr/bin/perl temporary. Fixup kident.sh to include all modules and calculate in-function.

--- base/projects/kident/kident.sh
+++ base/projects/kident/kident.sh
@@ -4,9 +4,8 @@
 #
 
 _get_ident(){
-	ident /boot/kernel/kernel |grep \$FreeBSD |\
-		awk '{print $3, $4, $5, $2, $6}' |\
-		sort -n |uniq
+	ident -q /boot/kernel/kernel /boot/kernel/*.ko |grep \$FreeBSD |\
+		awk '{print $3, $4, $5, $2, $6}' |sort -n |uniq
 }
 
 _get_commits(){
@@ -16,13 +15,14 @@ _get_commits(){
 
 _get_commits_top15(){
 	_get_ident |awk '{print $5}' |sort |uniq -c |\
-		awk '{print $1, $2}' |sort -nr |head -n15
+		awk '{print $1, $2}' |sort -nr |head -n15 |\
+		awk '{print $2"["$1"]"}' |column -x -c80
 }
 
 echo
-echo "Top 15 commits to the running kernel:"
+echo "Top 15 commits to the kernel + modules:"
 echo "==============================================================================="
-_get_commits_top15 |awk '{print $2"["$1"]"}' |column -x -c80
+_get_commits_top15
 echo
 echo "Number of contributions by committer:"
 echo "==============================================================================="
