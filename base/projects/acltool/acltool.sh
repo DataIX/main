28de3df8bce2097e8f7b7333d62412feb6e1b3e9
J. Hellenthal <jhellenthal@DataIX.net>
2011-10-03 00:49:18 -0400

    Add acltool(1).
    This eases support for setting ACLs and storing them for use later
    on when they might need to be restored due to upgrades etc...
    
    All files in the /etc/acls directory are generated with getfacl(1)
    and redirecting output to a file for editing after the new acl of the
    file has already been set using setfacl(1).
    
    This script also supports proccessing of the comments area for keyword
    "chflags" with one argument of a comma seperated "no-spaces" list of
    flags.
    
    Example:
    # chflags: schg,nodump,sappnd
    
    Since the setting of ACLs depends on schg and or uchg flags not being
    present these are removed prior to processing the .acl files located
    in the /etc/acls/ directory and reset using the above example. Be sure
    to properly edit your ACLs to add these flags if they are important for
    your secure operations.
    
    Exmaple .acl file:
    #######################################################################
    # file: /etc/acls
    # owner: root
    # group: wheel
    # chflags: sunlnk
    user::rwx
    group::---
    mask::---
    other::---
    #######################################################################

---
+++ base/projects/acltool/acltool.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -
+#
+# $Id$
+#
+# Copyright (c) 2011 Jason J. Hellenthal <jhell@DataIX.net>,
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+# aclfh = /etc/acls/*.acl access control list files
+# acltgt = /path/to/some/file-or-dir
+
+for aclfh in `find /etc/acls -type f -name \*.acl |sort`; do
+	for acltgt in `grep --null "^#.file:.." $aclfh |awk '{print $3}'`; do
+		if [ -f $acltgt -o -d $acltgt ]; then
+			test -e $acltgt && \
+			chflags -f noschg,nouchg $acltgt && \
+				setfacl -b -M $aclfh $acltgt && \
+				printf "$acltgt " && \
+				grep --null -nv ^\# $aclfh |xargs
+		fi
+		octets="`grep --null "^#.chflags:.." $aclfh |awk '{print $3}'`"
+		[ -n "$octets" ] && chflags -f $octets $acltgt
+	done
+done
