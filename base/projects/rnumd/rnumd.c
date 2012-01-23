de2d32a8a7c2869382deb764f642be1967773944
J. Hellenthal <jhell@DataIX.net>
2012-01-22 20:12:47 -0500

    Add random number generator for kern.randompid.
    Currently the only use this has is for kern.ranompid and other
    like entities. I have the intention to turn this into a daemon
    at some point but have not gotten to it.

---
+++ base/projects/rnumd/rnumd.c
@@ -0,0 +1,30 @@
+/*
+ *
+ * For use with crontab(1), crontab(5) or at(1)
+ *
+ * Usage: rnumd ;sysctl kern.randompid=$? >/dev/null
+ *
+ * $Id$
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	srandom(time(NULL) % 255);
+
+	unsigned int rseed = (unsigned int)random() % time(NULL);
+	srandom(rseed);
+	int r = ((random() % 1013) + (random() % 7));
+	
+	unsigned int lseed = (unsigned int)random() % time(NULL);
+	srandom(lseed);
+	int l = ((random() % 1327) + (random() % 3));
+
+	int e = (r % l);
+
+	return e;
+}
